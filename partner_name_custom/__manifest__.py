9762319dab530f29ae1015524b86ddfebfa9ed60
Franyer Hidalgo <fhidalgo.dev@gmail.com>
2021-09-09 17:17:11 -0400

    [ADD]: Módulo custom para nombres únicos de contactos

---
+++ partner_name_custom/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Nombre del partner unico',
+    'version': '14.0.1.1.0',
+    'author': 'Franyer Hidalgo - VE',
+    'website': '',
+    'contributors': ['Deyanira Jimenez'],
+    'category': '',
+    'license': 'AGPL-3',
+    'summary': 'Nombre del partner unico',
+    'depends': ['base',
+                ],
+    'data': [
+    ],
+    'demo': [],
+    'css': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
